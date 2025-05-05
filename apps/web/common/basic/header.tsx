417fb83f8efcd25161cf3f5dd0ddb80bf6e85c6c
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2025-05-05 11:51:15 +0530

    feat: logo by theme added

--- apps/web/common/basic/header.tsx
+++ apps/web/common/basic/header.tsx
@@ -14,6 +14,7 @@ const labels = {
 
 export default function header() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isUserPrefersDarkMode, setIsUserPrefersDarkMode] = useState(false);
 
   useEffect(() => {
     const getThemeFromStorage = getTheme();
@@ -22,24 +23,36 @@ export default function header() {
       setIsDarkMode(true);
       document.documentElement.classList.add("dark");
     }
+
+    // Check if the user has a system preference for dark mode
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    setIsUserPrefersDarkMode(mediaQuery.matches);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const toggleDarkMode = () => {
     setIsDarkMode((prevMode) => !prevMode);
     setTheme(isDarkMode ? "dark" : "light");
   };
+
+  const renderLogoPath = () => {
+    if (isDarkMode) return "/logo-light.svg";
+    return "/logo-dark.svg";
+  };
   return (
     <header className="w-full flex  pt-2 px-2">
       <div className="w-full flex justify-center">
-        <Logo path="/logo-jasoria.svg" width={310} height={55} alt="Jasoria" />
+        <Logo path={renderLogoPath()} width={286} height={55} alt="Jasoria" />
       </div>
       <div className="float-right">
-        <div className="flex items-center space-x-2">
-          <Switch id="airplane-mode" onClick={toggleDarkMode} />
-          <Label className="w-[34]" htmlFor="airplane-mode">
-            {isDarkMode ? labels.dark : labels.light}
-          </Label>
-        </div>
+        {!isUserPrefersDarkMode && (
+          <div className="flex items-center space-x-2">
+            <Switch id="airplane-mode" onClick={toggleDarkMode} />
+            <Label className="w-[34]" htmlFor="airplane-mode">
+              {isDarkMode ? labels.dark : labels.light}
+            </Label>
+          </div>
+        )}
       </div>
     </header>
   );
