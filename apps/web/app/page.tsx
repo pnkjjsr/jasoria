fddbe30a4b51e9d6838efc1748e2c4420db1fbcb
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2025-05-09 19:08:21 +0530

    chore: small structure fix and motion animation added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
+import * as motion from "motion/react-client";
+
 import Header from "@/common/headers/basic";
 import Nav from "@/common/nav/basic";
 
@@ -9,7 +11,20 @@ export default function Home() {
         <Nav />
       </div>
 
-      <main className="h-1000"></main>
+      <main className="h-1000">
+        <motion.div
+          initial={{ opacity: 0, scale: .8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.4,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+          }}
+        >
+          <div className="flex h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">Welcome to Jasoria</h1>
+          </div>
+        </motion.div>
+      </main>
     </>
   );
 }
--- apps/web/src/common/nav/basic.tsx
+++ apps/web/src/common/nav/basic.tsx
@@ -58,7 +58,7 @@ export default function NavigationMenuDemo() {
               <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li className="row-span-3">
                   <NavigationMenuLink asChild>
-                    <a
+                    <Link
                       className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                       href="/"
                     >
@@ -70,7 +70,7 @@ export default function NavigationMenuDemo() {
                         powerfully enhanced for results and beautifully simplest
                         designed.
                       </p>
-                    </a>
+                    </Link>
                   </NavigationMenuLink>
                 </li>
                 <ListItem href="/docs" title="Find Jobs">
