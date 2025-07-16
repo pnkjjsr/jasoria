c439c6dd6b560ca7d5859eb8d7b8325bbd9b4a4f
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2025-07-16 13:37:57 +0530

    hhb more service added

--- apps/web/app/hhb/page.tsx
+++ apps/web/app/hhb/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Wrench } from "lucide-react";
 import { PlugZap } from "lucide-react";
 import { AirVent } from "lucide-react";
+import { Armchair } from "lucide-react";
+import { GlassWater } from "lucide-react";
+import { ShowerHead } from "lucide-react";
+import { Flower } from "lucide-react";
 
 import { en as locale } from "@repo/shared/locale/index";
 import { HHB_TYPE } from "@repo/shared/const/hhb";
@@ -18,6 +22,14 @@ export default function HHB() {
       />
 
       <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
+        <FeatureForm
+          icon={<AirVent size={80} strokeWidth={0.5} />}
+          heading={locale.hhb_maid.heading}
+          sub={locale.hhb_maid.sub}
+          cta={locale.hhb_maid.cta}
+          type={HHB_TYPE.MAID}
+        />
+
         <FeatureForm
           icon={<Wrench size={80} strokeWidth={0.5} />}
           heading={locale.hhb_plumber.heading}
@@ -40,6 +52,38 @@ export default function HHB() {
           sub={locale.hhb_acservice.sub}
           cta={locale.hhb_acservice.cta}
           type={HHB_TYPE.AC_SERVICE}
+        />
+
+        <FeatureForm
+          icon={<Armchair size={80} strokeWidth={0.5} />}
+          heading={locale.hhb_carpenter.heading}
+          sub={locale.hhb_carpenter.sub}
+          cta={locale.hhb_carpenter.cta}
+          type={HHB_TYPE.CARPENTER}
+        />
+
+        <FeatureForm
+          icon={<GlassWater size={80} strokeWidth={0.5} />}
+          heading={locale.hhb_roservice.heading}
+          sub={locale.hhb_roservice.sub}
+          cta={locale.hhb_roservice.cta}
+          type={HHB_TYPE.RO_SERVICE}
+        />
+
+        <FeatureForm
+          icon={<ShowerHead size={80} strokeWidth={0.5} />}
+          heading={locale.hhb_geyser.heading}
+          sub={locale.hhb_geyser.sub}
+          cta={locale.hhb_geyser.cta}
+          type={HHB_TYPE.GEYSER}
+        />
+
+        <FeatureForm
+          icon={<Flower size={80} strokeWidth={0.5} />}
+          heading={locale.hhb_gardener.heading}
+          sub={locale.hhb_gardener.sub}
+          cta={locale.hhb_gardener.cta}
+          type={HHB_TYPE.GARDENER}
         />
       </div>
     </div>
--- packages/shared/src/const/hhb.tsx
+++ packages/shared/src/const/hhb.tsx
@@ -2,4 +2,9 @@ export const HHB_TYPE = {
   PLUMBER: "plumber",
   ELECTRICIAN: "electrician",
   AC_SERVICE: "acservice",
+  CARPENTER: "carpenter",
+  RO_SERVICE: "roservice",
+  GEYSER: "geyser",
+  MAID: "maid",
+  GARDENER: "gardener",
 };
