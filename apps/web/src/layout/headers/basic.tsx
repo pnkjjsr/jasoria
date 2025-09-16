545f11d91772b9f790765410fbe9fb3e323b0a6e
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2025-09-16 17:10:27 +0530

    new logo with mobiel and web sized condition.

--- apps/web/src/layout/headers/basic.tsx
+++ apps/web/src/layout/headers/basic.tsx
@@ -17,6 +17,7 @@ export default function Header() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isUserPrefersDarkMode, setIsUserPrefersDarkMode] = useState(false);
   const [scrollY, setScrollY] = useState(0);
+  const [isMobile, setisMobile] = useState(false);
 
   useEffect(() => {
     if (process.env.NODE_ENV === "production") analytics;
@@ -31,6 +32,9 @@ export default function Header() {
     // Check if the user has a system preference for dark mode
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
     setIsUserPrefersDarkMode(mediaQuery.matches);
+
+    if (window.innerWidth <= 480) setisMobile(true);
+    else setisMobile(false);
 
     const handleScroll = () => {
       const wheelY = document.body.scrollTop;
@@ -50,8 +54,8 @@ export default function Header() {
   };
 
   const renderLogoPath = () => {
-    if (isDarkMode) return "/logo-light.svg";
-    return "/logo-dark.svg";
+    if (isDarkMode) return "/logo-dark.png";
+    return "/logo-light.png";
   };
 
   return (
@@ -59,9 +63,14 @@ export default function Header() {
       className={`w-full flex py-2 px-2 animate-fade-in duration-700 ${scrollY > 50 ? "bg-white/90 dark:bg-[#1a1a1a]/90" : ""}`}
     >
       <div className="w-full flex align-left lg:justify-center md:py-4">
-        <Logo path={renderLogoPath()} width={164} height={55} alt="Jasoria" />
+        <Logo
+          path={renderLogoPath()}
+          width={isMobile ? 90 : 164}
+          height={isMobile ? 90 : 164}
+          alt="House Help Book"
+        />
       </div>
-      <div className="float-right flex justify-center space-x-2 items-start ">
+      <div className="absolute top-2 right-2 float-right flex justify-center space-x-2 items-start ">
         <Language />
 
         <div className="flex items-center space-x-2 pt-2">
